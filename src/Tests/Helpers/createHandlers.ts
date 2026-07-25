919c812ead833b2bc584a58dcbf1adb826e3951d
Victor DELEAU <deleau.victor.pro@gmail.com>
2026-07-25 10:48:48 +0200

    feat: GetSolutionPackagesQueryHandler, local aggregation consolidated per package

--- src/Tests/Helpers/createHandlers.ts
+++ src/Tests/Helpers/createHandlers.ts
@@ -1,5 +1,6 @@
 import { GetPackageManagementDiagnosticQueryHandler } from "@Application/Handlers/Packages/GetPackageManagementDiagnosticQueryHandler";
 import { GetProjectsTfmQueryHandler } from "@Application/Handlers/Projects/GetProjectsTfmQueryHandler";
+import { GetSolutionPackagesQueryHandler } from "@Application/Handlers/Packages/GetSolutionPackagesQueryHandler";
 import { SlnParser } from "@Infrastructure/Solution/SlnParser";
 import { SlnxParser } from "@Infrastructure/Solution/SlnxParser";
 import { BuildConfigDetector } from "@Infrastructure/Build/BuildConfigDetector";
@@ -9,9 +10,11 @@ import { PackagesConfigParser } from "@Infrastructure/Packages/PackagesConfigPar
 import { PackageVersionParser } from "@Infrastructure/Packages/PackageVersionParser";
 import { CpmDiagnosticService } from "@Infrastructure/Packages/CpmDiagnosticService";
 import { PackageManagementDiagnosticService } from "@Infrastructure/Packages/PackageManagementDiagnosticService";
+import { NuGetConfigResolver } from "@Infrastructure/Packages/NuGetConfigResolver";
+import { NuGetConfigParser } from "@Infrastructure/Packages/NuGetConfigParser";
 import { CsprojParser } from "@Infrastructure/Projects/CsprojParser";
 import { TfmResolver } from "@Infrastructure/Projects/TfmResolver";
-import { ILogger } from "@/Host/Application/Abstractions/Log/ILogger";
+import { type ILogger } from "@/Host/Application/Abstractions/Log/ILogger";
 
 const noOpLogger: ILogger = {
   Info: () => {},
@@ -51,4 +54,31 @@ export function createTfmHandler(): GetProjectsTfmQueryHandler {
   const tfmResolver = new TfmResolver(csprojParser, noOpLogger);
 
   return new GetProjectsTfmQueryHandler(slnParser, slnxParser, buildConfigDetector, buildConfigParser, tfmResolver);
+}
+
+export function createSolutionPackagesHandler(): GetSolutionPackagesQueryHandler {
+  const slnParser = new SlnParser();
+  const slnxParser = new SlnxParser();
+  const buildConfigDetector = new BuildConfigDetector();
+  const buildConfigParser = new BuildConfigParser(noOpLogger);
+  const packageReferenceParser = new PackageReferenceParser(noOpLogger);
+  const packagesConfigParser = new PackagesConfigParser(noOpLogger);
+  const packageVersionParser = new PackageVersionParser();
+  const cpmDiagnosticService = new CpmDiagnosticService(packageVersionParser);
+  const nuGetConfigParser = new NuGetConfigParser(noOpLogger);
+  const nuGetConfigResolver = new NuGetConfigResolver(nuGetConfigParser);
+  const csprojParser = new CsprojParser();
+  const tfmResolver = new TfmResolver(csprojParser, noOpLogger);
+
+  return new GetSolutionPackagesQueryHandler(
+    slnParser,
+    slnxParser,
+    buildConfigDetector,
+    buildConfigParser,
+    packageReferenceParser,
+    packagesConfigParser,
+    cpmDiagnosticService,
+    nuGetConfigResolver,
+    tfmResolver
+  );
 }
